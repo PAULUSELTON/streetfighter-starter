f47ecbda50affa772eb7d3c7e59c4a16f2cc633a
PAULUSELTON <PAULUSELTON@users.noreply.github.com>
2015-05-15 15:09:59 -0400

    Street-fighter final w cool pose

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
+    //console.log("hi");
     $('.ryu-still').hide();
     $('.ryu-ready').show();
   })
@@ -8,8 +9,11 @@ $(document).ready(function() {
     $('.ryu-still').show();
   })
   .mousedown(function() {
+   //console.log("hi2");
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-cool').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
   {'left': '1020px'},
@@ -21,12 +25,31 @@ $(document).ready(function() {
 );
  })
   .mouseup(function() {
+   //console.log("hi3");
+  $('.ryu-cool').hide();
     $('.ryu-throwing').hide();
   $('.ryu-ready').show();
+
   });
-  function playHadouken () {
+   $(document).keydown(function(event) {
+    if ( event.which == 88 ) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      //console.log("hi4");
+      $('.ryu-cool').show();
+    }
+       });
+ $(document).keyup(function(event) {
+    //console.log("hi5");
+  if ( event.which == 88 ) {
+   $('.ryu-cool').hide();
+    $('.ryu-still').show();
+         }
+       });
+ function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
-  });
+   });
